cca38a9bed23f31f64fe3e7184bdeaeddf663d1e
Jason Taetsch <jason@adigitalkingdom.com>
2015-11-05 10:37:05 -0500

    Removed dist/

--- dist/config/config.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('appConfig', [])
-    .value('serverUrl', 'https://api-staging.powerli.ne')
-    .value('adminUrl', 'https://api-staging.powerli.ne')
-    .value('marketplaceToken', 'TEST-MP7ByVfFJSMah8VMM0blIek6');
--- dist/scripts/services/users.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('app').factory('UserModel', function (JsModel) {
-  return JsModel.extend({
-
-  });
-});
