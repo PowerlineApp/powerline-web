0faf390425c3512120e3d70608fc5294df9af153
Jason Taetsch <jason@adigitalkingdom.com>
2015-11-05 11:12:54 -0500

    Ignore everything we dont need take two

--- app/config/config.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('appConfig', [])
-    .value('serverUrl', 'https://api.powerli.ne')
-    .value('adminUrl', 'https://api.powerli.ne')
-    .value('marketplaceToken', 'TEST-MP7ByVfFJSMah8VMM0blIek6');
